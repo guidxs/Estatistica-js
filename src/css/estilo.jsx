63c0bfbc25b57f829383716e39d48308ec0599a5
Guilherme <guilhermesobreiro9@gmail.com>
2023-11-07 11:36:18 -0300

    Final

---
+++ src/css/estilo.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const EstatEstilo =styled.div `
+
+.estatistica{
+    width: 100%;
+}
+`
---
+++ src/routers/Estatistica.jsx
@@ -0,0 +1,45 @@
+import {} from 'react'
+import {VictoryChart, VictoryBar, VictoryAxis, VictoryTheme} from 'victory'
+import { EstatEstilo } from '../css/estilo';
+
+function Estatistica(){
+
+    const dados =[
+        {Produto:1, Valor:2000},
+        {Produto:2, Valor:1200},
+        {Produto:3, Valor:1100},
+        {Produto:4, Valor:1000},
+    ];
+
+
+    return(
+        <>
+        <EstatEstilo>
+        <div className='estatistica'>
+
+        </div>
+        <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={30}
+        >
+        <VictoryAxis
+            tickValues={[1,2,3,4]}
+            tickFormat={["Produto 1","Produto 2","Produto 3", "Produto 4"]}
+        />
+
+        <VictoryAxis
+            dependentAxis
+            tickFormat={(x)=>(`R$ ${x /100}`)}
+        />
+
+            <VictoryBar
+            data={dados}
+            x="Produto"
+            y="Valor"
+            />
+        </VictoryChart>
+        </EstatEstilo>
+        </>
+    )
+}
+export default Estatistica
